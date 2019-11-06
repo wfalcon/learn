33fbbeb68a8249c43d94255f37c76abff4f514d6
wfalcon@mail.ru <wfalco@mail.ru>
2019-11-07 01:33:26 +0300

    templates

--- learn.py
+++ learn.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
-a = 10
-b = 5
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
 
-if a > b:
-    print("A больше B")
-    print(a - b)
-else:
-    print("B больше или равно A")
-    print(b - a)
-
-print("Конец")
-
-def open_file(filename):
-    print("Чтение файла", filename)
-    with open(filename) as f:
-        return f.read()
-        print("Готово")
+print('\n'.join(access_template).format(5))
