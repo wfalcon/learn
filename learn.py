0db121ba03a807811709848170079381603ffc19
wfalcon@mail.ru <wfalco@mail.ru>
2019-11-07 01:56:48 +0300

    templates argv

--- learn.py
+++ learn.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+from sys import argv
+
+interface = argv[1]
+vlan = argv[2]
+
 access_template = ['switchport mode access',
                    'switchport access vlan {}',
                    'switchport nonegotiate',
                    'spanning-tree portfast',
                    'spanning-tree bpduguard enable']
 
-print('\n'.join(access_template).format(5))
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
