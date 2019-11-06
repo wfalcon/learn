da338bdf42a5e21d0d513d8cfae812a1c8f73a6c
wfalcon@mail.ru <wfalco@mail.ru>
2019-11-07 02:04:45 +0300

    templates argv input

--- learn.py
+++ learn.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-from sys import argv
-
-interface = argv[1]
-vlan = argv[2]
+interface = input('Enter interface type and number: ')
+vlan = input('Enter VLAN number: ')
 
 access_template = ['switchport mode access',
                    'switchport access vlan {}',
@@ -10,5 +8,7 @@ access_template = ['switchport mode access',
                    'spanning-tree portfast',
                    'spanning-tree bpduguard enable']
 
+print('\n' + '-' * 30)
 print('interface {}'.format(interface))
 print('\n'.join(access_template).format(vlan))
+
